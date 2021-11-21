9c3d76c9f6200a15923d5d75b822f210087356f9
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:04:08 +0000

    Added ?ExtensionInterface

--- EveryWorkflowCustomerBundle.php
+++ EveryWorkflowCustomerBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\CustomerBundle;
 
 use EveryWorkflow\CustomerBundle\DependencyInjection\CustomerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowCustomerBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new CustomerExtension();
     }
